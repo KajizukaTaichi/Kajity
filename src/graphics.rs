49aa7d481a56fa54f24293f1065248f8c40321f9
梶塚太智 <kajizukataichi@outlook.jp>
2024-04-19 16:06:38 +0900

    Update draw

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,8 +1,11 @@
+use image::GenericImageView;
+use image::Pixel;
 use minifb::{Window, WindowOptions};
+use std::path::Path;
 
 pub struct Game {
     window: Window,
-    buffer: Vec<u32>
+    buffer: Vec<u32>,
 }
 
 impl Game {
@@ -10,19 +13,14 @@ impl Game {
         let width = 800;
         let height = 600;
 
-        let window = Window::new(
-            &title,
-            width,
-            height,
-            WindowOptions::default(),
-        )
-        .unwrap_or_else(|e| {
-            panic!("{}", e);
-        });
+        let window =
+            Window::new(&title, width, height, WindowOptions::default()).unwrap_or_else(|e| {
+                panic!("{}", e);
+            });
 
         Game {
             window,
-            buffer: vec![0; width * height]
+            buffer: vec![0; width * height],
         }
     }
 
@@ -31,12 +29,32 @@ impl Game {
             let size = self.window.get_size();
             let _ = self.window.update_with_buffer(&self.buffer, size.0, size.1);
         }
-    }    
+    }
 
-    pub fn draw(&mut self, color: u32, start: (usize, usize), end:(usize, usize)) {
-        let (width ,height) = self.window.get_size();
+    pub fn image(&mut self, path: &str, point: (u32, u32)) {
+        let img = image::open(Path::new(path)).unwrap();
+        let (width, height) = img.dimensions();
+        let mut buffer = img.to_rgba8().into_raw();
+
+        let x = point.0;
+        let y = point.1;
+
+        for j in 0..height {
+            for i in 0..width {
+                let pixel = img.get_pixel(i, j);
+                let rgba = pixel.channels();
+                let color = (rgba[0] as u32) << 16 | (rgba[1] as u32) << 8 | rgba[2] as u32;
+                let index = (y + j) * self.window.get_size().0 as u32 + (x + i);
+                buffer[index as usize] = color as u8;
+            }
+        }
+        self.buffer = buffer.iter().map(|x|*x as u32).collect();
+    }
+
+    pub fn draw(&mut self, color: u32, start: (usize, usize), end: (usize, usize)) {
+        let (width, height) = self.window.get_size();
         let mut buffer: Vec<u32> = self.buffer.clone();
-        
+
         for y in start.1..=end.1 {
             for x in start.0..=end.0 {
                 if x < width && y < height {
@@ -45,8 +63,10 @@ impl Game {
                 }
             }
         }
-        
-        self.window.update_with_buffer(&self.buffer, width, height).unwrap();
+
+        self.window
+            .update_with_buffer(&self.buffer, width, height)
+            .unwrap();
         self.buffer = buffer.clone();
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use graphics::Game;
 
 fn main() {
     let mut game = Game::new("test".to_string());
-    game.draw(0x0000FF, (0,0), (500,500));
-    game.draw(0x0000AA, (0,0), (200,800));
+    game.draw(0x0000FF, (10, 10), (50, 50));
+    game.draw(0x00BBAA, (20, 80), (50, 90));
+    game.image("C:/Users/admin/OneDrive/画像/logic.png", (1,10));
     game.run();
-    
 }
